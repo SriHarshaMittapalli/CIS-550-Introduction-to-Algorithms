598e5b459f42fba3a3fc13919fc75e94c2c7ad89
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-10 12:30:36 -0400

    Add files via upload

---
+++ Assignment-5/Q1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestPalindrome(self, s):
+        #input: string
+        #output: longest Palindrome
+            return longest
+
+if __name__ == "__main__":
+    test = Solution()
+    res = test.longestPalindrome("abcdzdcab")
+    if res == 'cdzdc':
+        print('PASS')
---
+++ Assignment-5/Q2.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def pick(self, nums):
+        """
+        input: int list, numbers of coins
+        output: maximum number
+        """
+        return maximum
+
+
+if __name__ == "__main__":
+    res = Solution().pick([1, 2, 3, 4, 5, 6, 7])
+    print(res)
+    if res == 15:
+        print('PASS')
