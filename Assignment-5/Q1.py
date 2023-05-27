097195a4f9a22e232ae89d0d843f03113d5888e4
Sri Harsha Mittapalli <53350799+SriHarshaMittapalli@users.noreply.github.com>
2023-05-27 17:10:20 -0400

    Add files via upload

--- Assignment-5/Q1.py
+++ Assignment-5/Q1.py
@@ -2,7 +2,16 @@ class Solution:
     def longestPalindrome(self, s):
         #input: string
         #output: longest Palindrome
-            return longest
+        # Time complexity using DP is O(n^2), Time complexity using brute-force attack is O(n^3)
+        input_size, input_index = len(s), 0; dynamic_programming_table, longest_string = [[0] * input_size for i in range(input_size)], "null"
+        while input_index < input_size: dynamic_programming_table[input_index][input_index], longest_string = 1, s[input_index]; input_index = input_index + 1
+        for input_index in range(input_size - 1):
+            if s[input_index] == s[input_index + 1]: dynamic_programming_table[input_index][input_index + 1], longest_string = 1, s[input_index:input_index + 2]
+        for length_of_palindrome in range(3, input_size+1):
+            for input_row in range(input_size-length_of_palindrome + 1):
+                input_column = input_row + length_of_palindrome - 1
+                if (s[input_row] == s[input_column]) and (dynamic_programming_table[input_row + 1][input_column - 1]): dynamic_programming_table[input_row][input_column], longest_string = 1, s[input_row:input_column + 1]
+        return longest_string
 
 if __name__ == "__main__":
     test = Solution()
--- Assignment-5/Q2.py
+++ Assignment-5/Q2.py
@@ -4,9 +4,13 @@ class Solution(object):
         input: int list, numbers of coins
         output: maximum number
         """
+        size_of_list = len(nums)
+        dynamic_programming_1, dynamic_programming_1[0], dynamic_programming_1[1], dynamic_programming_index1 = [0] * size_of_list, nums[0], max(nums[0], nums[1]), 2
+        while dynamic_programming_index1 < size_of_list: dynamic_programming_1[dynamic_programming_index1], dynamic_programming_index1 = max(dynamic_programming_1[dynamic_programming_index1 - 2]+nums[dynamic_programming_index1], dynamic_programming_1[dynamic_programming_index1 - 1]), dynamic_programming_index1 + 1
+        dynamic_programming_2, dynamic_programming_2[0], dynamic_programming_2[1], dynamic_programming_index2 = [0] * size_of_list, 0, nums[1], 2
+        while dynamic_programming_index2 < size_of_list: dynamic_programming_2[dynamic_programming_index2], dynamic_programming_index2 = max(dynamic_programming_2[dynamic_programming_index2 - 2]+nums[dynamic_programming_index2], dynamic_programming_2[dynamic_programming_index2 - 1]), dynamic_programming_index2 + 1
+        maximum = max(dynamic_programming_1[-2], dynamic_programming_2[-1])
         return maximum
-
-
 if __name__ == "__main__":
     res = Solution().pick([1, 2, 3, 4, 5, 6, 7])
     print(res)
